f261f89bc67336621925e6651c119de240c952df
Spinkk <mpink@uos.de>
2021-04-13 13:44:34 +0200

    traversal complete and new functions to finish

--- deep_CFR_traversal.py
+++ deep_CFR_traversal.py
@@ -1,3 +1,4 @@
+import copy
 
 def get_env_cpy(orig_env):
     env_cpy = create_env()
@@ -6,18 +7,28 @@ def get_env_cpy(orig_env):
     return env_cpy
 
 
-def trainings_loop():
+def get_history_cpy(orig_history):
+    """copies a list"""
 
-    for CFR_iteration:
+    return copy.deepcopy(orig_history)
 
-        traversal(env, obs)
+def get_info_state(obs, history):
+    """ Transforms the observation dictionary from clubs env and the history list to an info state (input to the ANN model)"""
+    pass
+
+def save_to_memory(type, player, info_state, iteration, values):
+
+    """This function saves stuff to memory"""
+
+    ### bring info state and values and iteration into a useful data structure
+
+    ### save this data structure, e.g. a dict (for instance each iteration gets its own file?)
+
+    pass
 
 
 def deep_CFR(env, val_net, strat_net, CFR_iterations, num_traversals, num_players):
-    """
-
-    Does some stuff.
-
+    """"
     Parameters
     ----------
     env : gym.env instance
@@ -37,9 +48,7 @@ def deep_CFR(env, val_net, strat_net, CFR_iterations, num_traversals, num_player
     Returns
     -------
     strat_net : tf.keras.model class
-                The trained strategy network.
-
-    """
+                The trained strategy network.""""
 
     # initialize ANNs and memories
 
@@ -49,7 +58,7 @@ def deep_CFR(env, val_net, strat_net, CFR_iterations, num_traversals, num_player
                 # collect data from env via external sampling
                 env = create.env()
                 obs = env.reset()
-                payoff = traverse(e)
+                traverse(env, obs, history, p, t)
 
             # initialize new value network (if not first iteration) and train with val_mem_p
             # (only used for prediction of the next regret values)
@@ -58,12 +67,11 @@ def deep_CFR(env, val_net, strat_net, CFR_iterations, num_traversals, num_player
         # train the strat_net with strat_mem
         train_strat_net()
 
-    return
+    return 0
 
-def traverse(env, obs, history, traverser, advantage_mem_traverser, strat_mem, CFR_iteration):
+def traverse(env, obs, history, traverser, CFR_iteration):
     """
-    Does some stuff.
-    # input(histroy, traverser, val_model_0, val_model_1, val_mem_trav, strat_mem, t)
+    # input(history, traverser, val_model_0, val_model_1, val_mem_trav, strat_mem, t)
 
     Parameters
     ----------
@@ -96,11 +104,11 @@ def traverse(env, obs, history, traverser, advantage_mem_traverser, strat_mem, C
     None
 
     """
-    state_terminal = all(obs['active'])
-    if not state_terminal:
+    state_terminal = not all(obs['active'])
+    if state_terminal:
         # game state: end
         # calculate traversers payoff
-        traverser_payoff = obs['payoff'][traverser] - obs['commitment'][traverser]
+        traverser_payoff = obs['payoff'][traverser] - obs['commitment'][traverser] # gain or loss
 
         return traverser_payoff
 
@@ -119,29 +127,45 @@ def traverse(env, obs, history, traverser, advantage_mem_traverser, strat_mem, C
         # his val_net via regret matching (used for weighting when calculating
         # the advantages)
         # call model on observation, no softmax
-        obs = preprocess_obs(obs)
-        strategy = env.dealer.agents[traverser].act(obs, strategy=True)
+        info_state = get_info_state(obs, history)
+        strategy = env.dealer.agents[traverser].act(info_state, strategy=True)
 
         # 2.
         # iterate over all actions and do traversals starting from each actions
         # subsequent history
+        values = []
         for a in range(len(strategy)):
             # cpy environment
             # take selected action within copied environment
+            history_cpy = get_hist_cpy(history) # copy bet size history
             env_cpy = get_env_cpy(env)
             obs = env_cpy.step(a)
-            traverser_payoff = traverse(env_cpy, obs, ...)
 
-            return traverser_payoff
+            history_cpy.append(a) # add bet to bet history
+
+
+            traverser_payoff = traverse(env_cpy, obs, history_cpy, traverser, CFR_iteration)
+            values.append(traverser_payoff)
+
+            #return traverser_payoff
 
         # 3.
         # use returned payoff for advantage/regret computation
-        for a in range(len(actions)):
+        advantages = []
+        for a in range(len(strategy)):
             # compute advantages of each action
+            advantages.append(values[a] - np.sum(strategy.numpy() * np.array(values)))
 
         # 4.
         # append Infoset, action_advantages and CFR_iteration t to advantage_mem_traverser
 
+        save_to_memory(
+        type = "value",
+        player = traverser,
+        info_state = info_state,
+        iteration = CFR_iteration,
+        values = advantages
+        )
     else:
         # game state: traversers opponent has to take an action
 
@@ -149,15 +173,27 @@ def traverse(env, obs, history, traverser, advantage_mem_traverser, strat_mem, C
         # compute strategy (next action) from the Infoset of the opponent and his
         # val_net via regret matching
         # call model on observation, no softmax
-        obs = preprocess_obs(obs)
-        strategy = env.dealer.agents[3-traverser].act(obs, strategy=True)
+        info_state = get_info_state(obs, history)
+
+        non_traverser = 3 - traverser
+        strategy = env.dealer.agents[non_traverser].act(info_state, strategy=True) # env.act(orig_obs, strategy = True) probably is what works
 
         # 2.
         # append Infoset, action_probabilities and CFR_iteration t to strat_mem
-
+        save_to_memory(
+        type = "strategy",
+        player = non_traverser,
+        info_state = info_state,
+        iteration = CFR_iteration,
+        values = strategy.numpy()
+        )
         # 3.
         # copy env and take action according to action_probabilities
+        dist = tfp.distributions.Categorical(probs = strategy.numpy())
 
-        traverser_payoff = traverse()
-
-        return traverser_payoff
+        sampled_action = dist.sample((1)).numpy()
+        action = env.act(info_state)
+        obs = env.step(action)
+        # update history
+        history.append(action)
+        return traverse(env, obs, history, traverser, CFR_iteration)
