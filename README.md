9493605313892b57552c324be05e99aa49afd25c
TheExplorer95 <j.bajorath@outlook.de>
2021-04-26 23:39:57 +0200

    finish touch

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Learning-Deep-Reinforcement-Learning
+# Combining Reinforcement Learning, Deep Learning and Game theory approaches in building a poker bot
+
+This is Janosch Bajoraths and Mathis Pinks project for the the deep Reinforcement Learning class (WS20/21) at the University of Osnabrück.
+
+## Dependencies
+
+There are some important packages like gym, clubs, clubs_gym, ray and tensorflow that you should have installed on your computer to run the code. The best way to set up your environment is by using conda to create a new environment from the environment.yml file and then install some further requirements from the requirements.txt file via pip.
+
+We use a poker game implementation from the clubs_gym package and ray for multi-threaded sampling during CFR traversals.
+
+If something doesn't work feel free to contact us!!!
+
+## Content
+
+We have **2 python scripts** and **2 jupyter notbooks** that are intended for **end user application**:
+1. main
+> The core deep CFR algorithm implemented according to the [deep CFR paper](https://arxiv.org/abs/1811.00164]). Set preferred parameters within the script (preset ones are standard ones used by us) and run it from your terminal. It will train and save both players trained advantage models and loss during each CFR iteration as well as the strategy model at the end of the CFR algorithm. Creates memory files for the data that was produced during the game traversal. 
+
+2. test_poker_bot
+> A Jupyter Notebook that is used to evaluate pretrained bots, just define your model paths and you are good to go.
+> Includes different **rendering modis**: The *Terminal* one outputs the current game into the Notebook terminal, in the *Display* one a browser can be used to render the current game (+ live plot of rewards in terminal) and you can also switch it off.
+
+3. play_against_bot
+> With this Notebook you can play against a trained bot, while getting 'assistance' from the bot by receiving information about the choices he would have made in your situation.
+> Also includes different rendering modis. 
+
+4. eval_bot
+> Python script to evaluate several bots at once. Just set your parameters at the top and you are good to got (predefined parameters also work).
+
+**Oother important scripts:**
+1. deep_CFR_algorithm
+> Ray implementation of the deep CFR Algorithm from the [deep CFR paper](https://arxiv.org/abs/1811.00164]).
+
+2. deep_CFR_model
+> The tensorflow models used by the poker agents.
+
+3. poker_agent
+> Clubs_gym poker agents.
+
+**Utils:**
+1. utils
+2. memory_utils
+3. training_utils
+4. jupyter_nb_utils
+5. eval_utils
+
+**Folders:**
+1. trained_models
+> Contains tensorflow models of pretrained bots.
+
+2. results/eval
+> Contains evaluation data of trained advantage and strategy networks. (When train was used even more)
+
+**Documentaion**
+
+1. {proejct_report}
+
--- memory_utils.py
+++ memory_utils.py
@@ -1,37 +1,16 @@
-import tensorflow as tf
 import h5py
 import numpy as np
 import random
+
 
 def flatten_data_for_memory(info_state, iteration, values):
     """
     Flattens data to store into a memory object.
     """
-    #print(values)
-
-    #print(values, info_state)
-
-
-    """
-    BIG PROBLEM: (TODO) values is sometimes a list and sometimes an array of shape (1,5) and sometimes it is a list of length
-
-
-    """
-
-    #### TODO fix that values is always the same shape and the same object type (currently isn't)
-    try:
-
-        values = np.reshape(values, (values.shape[1],))
-    except:
-        values = np.array(values)
-
-    flattened_data = np.concatenate(
-                    [
-                        info_state[0][0].flatten(),
-                        info_state[1].flatten(),
-                        np.array([iteration]),
-                        np.array(values)
-                    ],  axis = 0)
+    flattened_data = np.concatenate([*[info_state[0][i].flatten() for i in range(len(info_state[0]))],
+                                    info_state[1].flatten(),
+                                    np.array([iteration]),
+                                    np.array(values)], axis=0)
 
     return flattened_data
 
@@ -41,69 +20,59 @@ class MemoryWriter(object):
     """
     Keeps track of how many items are already processed for this memory.
     It's main purpose is to store the generated data with the save_to_memory method
+
+    vector_length = num_hole_cards + num_flop_cards + max_bet_num + num_actions + 1
     """
 
-
     def __init__(self, max_size, vector_length, flatten_func, file_name):
-
         self.max_size = max_size
-
-        self.vector_len = vector_length # flatten the input that we want to store and take len
-
+        self.vector_len = vector_length  # flatten the input that we want to store and take len
         self.flatten_func = flatten_func
-
-        self.counter = np.array([0,0]) # can't assign a single number to a dataset in hdf5 files
-
+        self.counter = np.array([0, 0])  # can't assign a single number to a dataset in hdf5 files
         self.file_name = file_name
 
         # load previous counter or initiate memory file
         try:
             with h5py.File(self.file_name, "r") as hf:
                 self.counter = np.array(hf.get("counter"))
-            print("previous counter loaded")
+            print("[INFO] - previous counter loaded")
 
-        except:
+        except Exception:
             # create new dataset file with a counter and an array
             with h5py.File(self.file_name, "w") as hf:
-
-                hf.create_dataset("counter", data= self.counter)
-                hf.create_dataset("data", (self.max_size, self.vector_len), dtype = np.float32)
-            print(f"new counter set and dataset of size {(self.max_size, self.vector_len)} is initiated.")
+                hf.create_dataset("counter", data=self.counter)
+                hf.create_dataset("data", (self.max_size, self.vector_len), dtype=np.float32)
+            print(f"[INFO] - new counter set and dataset of size {(self.max_size, self.vector_len)} is initiated.")
 
     def save_to_memory(self, data):
         """
-        Takes a list of tuples (info_state, iteration, values) and stores each to the memory hdf5 file.
+        Takes a list of tuples (info_state, iteration, values) and stores each
+        to the memory hdf5 file.
 
         Uses Reservoir sampling for samples that exceed the specified max_size.
         """
-        with h5py.File(self.file_name, 'r+') as hf:
+        if not len(data):
+            return None
 
+        with h5py.File(self.file_name, 'r+') as hf:
             # store each tuple in data
             for info_state, iteration, values in data:
-
-                self.counter[1] += 1
-
-                # save counter only every 100 steps
-                if not self.counter[1]%100:
-                    hf.get("counter")[1] = self.counter[1]
+                hf.get("counter")[1] = self.counter[1]
 
                 # if reservoir is not yet full, simply add new sample
                 if self.counter[1] < self.max_size:
-
                     flattened_data = self.flatten_func(info_state, iteration, values)
-
-                    #print(flattened_data.shape)
-                    #print(flattened_data)
-                    hf.get("data")[self.counter[1]] = flattened_data # fill empty row with data
+                    hf.get("data")[self.counter[1]] = flattened_data  # fill empty row with data
 
                 # if reservoir is full already, randomly replace or not replace old data
                 else:
-                    idx = random.randint(0, self.counter[1]) # index to replace (or not)
+                    idx = random.randint(0, self.counter[1])  # index to replace (or not)
                     if idx < self.max_size:
 
                         flattened_data = self.flatten_func(info_state, iteration, values)
-
-                        hf.get("data")[idx] =  flattened_data # replace the old data at idx with the new sample
+                        hf.get("data")[idx] = flattened_data  # replace the old data at idx with the new sample
 
                     else:
-                        pass # data is not stored in favor of old data
+                        pass  # data is not stored in favor of old data
+
+                self.counter[1] += 1
